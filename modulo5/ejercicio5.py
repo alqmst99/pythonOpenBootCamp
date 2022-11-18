9e1c7b9fc6580f6cd2dff38e304fc62c0aaf8b4e
nahuel pierini <nahuel.pierini90@hotmail.com>
2022-11-18 00:45:14 -0300

    python 5.00.01

--- modulo5/ejercicio5.py
+++ modulo5/ejercicio5.py
@@ -1,11 +1,22 @@
 print("es año biciesto?")
 print()
-anio = int(input("ingrese el año: "))
-def anioBiciesto(fecha):
-    valor= fecha%4
-    if valor==0:
-        print("el año", fecha,"es biciesto")
-    else:
-        print("el año", fecha, "no es biciesto es normal")
+i=1
+while  i == 1 or i==0:
+ anio = int(input("ingrese el año: "))
 
-anioBiciesto(anio)
+    def anioBiciesto (fecha) :
+        valor = fecha % 4
+        if valor == 0:
+            print("el año", fecha,"es biciesto")
+        elif valor!=0:
+
+
+
+anioBiciesto ( anio )
+
+elif valor != 0:
+print("el año", fecha, "no es biciesto es normal")
+i = int(input("desea repetir? 1 si, 0 no")) - 3
+elif i != 0 or i != 1:
+print("ingrese in valor valido")
+i = int(input("vuelva a ingresar la opcion, 1 si, 0 no ")) - 3
